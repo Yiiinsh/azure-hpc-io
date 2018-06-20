7c2462c0a6f3a368b71a0b9b3bc12f8557ae4c76
Shaohan Yin <shaohan.yin@gmail.com>
2018-06-21 00:10:41 +0100

    [AZURE] single blob, master get and bcast

--- azure-blob-bench/benchblob.py
+++ azure-blob-bench/benchblob.py
@@ -1,5 +1,4 @@
-#! /usr/bin/env python3 
-
+#! /usr/bin/env python3
 ' Benchmarking Azure blob performance for HPC purpose'
 
 import os, sys, configparser
@@ -21,10 +20,17 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 processor_name = MPI.Get_processor_name()
 
+# Azure
+block_blob_service = BlockBlobService(account_name=config_azure['account_name'], account_key=config_azure['account_key'])
+block_blob_service.get_container_acl(config_azure['source_container_name'])
+blob_size = block_blob_service.get_blob_properties(config_azure['source_container_name'], config_azure['source_blob_name']).properties.content_length
+
 def bench_blob():
 	print('Rank {0} of {1}. Processor[{2}]'.format(rank, size, processor_name))
 
 	bench_blob_get_with_single_blob_single_container()
+
+	bench_blob_get_with_master_download_and_broadcast()
 
 
 def bench_blob_get_with_single_blob_single_container():
@@ -34,18 +40,15 @@ def bench_blob_get_with_single_blob_single_container():
 	Return:
 		type[] : source stream
 	'''
-
-	block_blob_service = BlockBlobService(account_name=config_azure['account_name'], account_key=config_azure['account_key'])
-	block_blob_service.get_container_acl(config_azure['source_container_name'])
-	blob_size = block_blob_service.get_blob_properties(config_azure['source_container_name'], config_azure['source_blob_name']).properties.content_length
-
+	# Matrics to be recorded
 	max_read = 0
 	min_read = sys.maxsize
 	avg_read = 0
+
 	for _ in range(0, int(config_bench['repeat_time'])):
 		comm.Barrier()
 		start = MPI.Wtime()
-		data = block_blob_service.get_blob_to_bytes(config_azure['source_container_name'], config_azure['source_blob_name'])
+		data = block_blob_service.get_blob_to_text(config_azure['source_container_name'], config_azure['source_blob_name'])
 		end = MPI.Wtime()
 		comm.Barrier()
 
@@ -72,6 +75,7 @@ def bench_blob_get_with_single_blob_single_container():
 		avg_read = round(avg_read / int(config_bench['repeat_time']), 3)
 
 		print('-------- Single Blob, Single Container --------')
+		print('-------- Repeat {0} times --------'.format(config_bench['repeat_time']))
 		print('-------- {0} KiB Inputs on {1} Processes --------'.format(round(blob_size/ 1024, 3), size))
 		print('Max {0} s, Min {1} s, Avg {2} s'.format(max_read, min_read, avg_read))
 	
@@ -84,8 +88,56 @@ def bench_blob_get_with_multiple_blob_single_container():
 def bench_blob_get_with_multiple_blob_multiple_container():
 	pass
 
-def bench_blob_get_with_master_download_and_distribute():
-	pass
+def bench_blob_get_with_master_download_and_broadcast():
+	'''
+	Benchmarking Blob get with master-read pattern
+
+	Return:
+		type[] : source stream
+	'''
+	# Matrics to be recorded
+	max_read = 0
+	min_read = sys.maxsize
+	avg_read = 0
+
+	for _ in range(0, int(config_bench['repeat_time'])):
+		comm.Barrier()
+		start = MPI.Wtime()
+		if RANK_MASTER == rank:
+			data = block_blob_service.get_blob_to_text(config_azure['source_container_name'], config_azure['source_blob_name'])
+		else :
+			data = None
+		data = comm.bcast(data, RANK_MASTER)
+		end = MPI.Wtime()
+		comm.Barrier()
+
+		# Metrics
+		read_time = np.zeros(1)
+		max_read_time = np.zeros(1)
+		min_read_time = np.zeros(1)
+		avg_read_time = np.zeros(1)
+		read_time[0] = end - start
+		comm.Reduce(read_time, max_read_time, MPI.MAX, RANK_MASTER)
+		comm.Reduce(read_time, min_read_time, MPI.MIN, RANK_MASTER)
+		comm.Reduce(read_time, avg_read_time, MPI.SUM, RANK_MASTER)
+
+		if RANK_MASTER == rank:
+			max_read_time[0] = round(max_read_time[0], 3)
+			min_read_time[0] = round(min_read_time[0], 3)
+			avg_read_time[0] = round(avg_read_time[0] / size, 3)
+
+			max_read = max(max_read_time[0], max_read)
+			min_read = min(min_read_time[0], min_read)
+			avg_read += avg_read_time[0]
+
+	if RANK_MASTER == rank:
+		avg_read = round(avg_read / int(config_bench['repeat_time']), 3)
+
+		print('-------- Single Blob, Master Broadcast --------')
+		print('-------- Repeat {0} times --------'.format(config_bench['repeat_time']))
+		print('-------- {0} KiB Inputs on {1} Processes --------'.format(round(blob_size/ 1024, 3), size))
+		print('Max {0} s, Min {1} s, Avg {2} s'.format(max_read, min_read, avg_read))
+
 
 if __name__ == '__main__':
 	bench_blob()
