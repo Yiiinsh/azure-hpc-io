07692940cc15d4f5ed006c9a8f0314153b871917
Shaohan Yin <shaohan.yin@gmail.com>
2018-06-27 23:53:16 +0100

    [AZURE] file get bench

--- azure_file_bench/benchfile.py
+++ azure_file_bench/benchfile.py
@@ -1,15 +1,15 @@
 #! /usr/bin/env python3
-' Benchmarking Azure blob performance for HPC purpose'
+' Benchmarking Azure file performance for HPC purpose'
 
 import os, sys, configparser
 import numpy as np
 from mpi4py import MPI
-from azure.storage.blob import BlockBlobService
+from azure.storage.file import FileService
 from common import common
 
-def bench_blob_get_with_single_blob_single_container():
+def bench_file_get():
 	'''
-	Benchmarking Blob get with multiple access on a single blob within a single container
+	Benchmarking File get with multiple access on a single file within a single share
 
 	Return:
 	 type[] : source stream
@@ -23,9 +23,9 @@ def bench_blob_get_with_single_blob_single_container():
 	rank, size, _ = common.get_mpi_env()
 
 	# Azure
-	block_blob_service = BlockBlobService(account_name=config_azure['account_name'], account_key=config_azure['account_key'])
-	block_blob_service.get_container_acl(config_azure['source_container_name'])
-	blob_size = block_blob_service.get_blob_properties(config_azure['source_container_name'], config_azure['source_blob_name']).properties.content_length
+	file_service = FileService(account_name=config_azure['account_name'], account_key=config_azure['account_key'])
+	file_service.get_share_acl(config_azure['source_share_name'])
+	file_size = file_service.get_file_properties(config_azure['source_share_name'], None, config_azure['source_file_name']).properties.content_length
 
 	# Metrics
 	max_read, min_read, avg_read = common.init_bench_metrics()
@@ -33,7 +33,7 @@ def bench_blob_get_with_single_blob_single_container():
 	for _ in range(0, int(config_bench['repeat_time'])):
 		MPI.COMM_WORLD.Barrier()
 		start = MPI.Wtime()
-		data = block_blob_service.get_blob_to_text(config_azure['source_container_name'], config_azure['source_blob_name'])
+		data = file_service.get_file_to_text(config_azure['source_share_name'], None, config_azure['source_file_name'])
 		end = MPI.Wtime()
 		MPI.COMM_WORLD.Barrier()
 
@@ -48,12 +48,12 @@ def bench_blob_get_with_single_blob_single_container():
 	avg_read = round(avg_read / int(config_bench['repeat_time']), 3)
 
 	if 0 == rank:
-		print('-------- Single Blob, Single Container --------')
+		print('-------- Single File, Single Share --------')
 		print('-------- Repeat {0} times --------'.format(config_bench['repeat_time']))
-		print('-------- {0} KiB Inputs on {1} Processes --------'.format(round(blob_size/ 1024, 3), size))
+		print('-------- {0} KiB Inputs on {1} Processes --------'.format(round(file_size/ 1024, 3), size))
 		print('Max {0} s, Min {1} s, Avg {2} s'.format(max_read, min_read, avg_read))
 	
 	return data
 
 if __name__ == '__main__':
-	bench_blob_get_with_single_blob_single_container()
+	bench_file_get()
--- bench.py
+++ bench.py
@@ -7,6 +7,7 @@ from mpi4py import MPI
 from common import common
 from cirrus_lustre_bench import benchcirrus
 from azure_blob_bench import benchblob
+from azure_file_bench import benchfile
 
 def bench():
 	# Configurations
@@ -23,6 +24,8 @@ def bench():
 			benchcirrus.bench_cirrus_get_with_posix_io()
 		elif target == 'azure_blob':
 			benchblob.bench_blob_get_with_single_blob_single_container()
+		elif target == 'azure_file':
+			benchfile.bench_file_get()
 	
 
 if __name__ == '__main__':
