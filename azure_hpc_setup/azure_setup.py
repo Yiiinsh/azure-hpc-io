09b2761c32dc628375f7e4c585cc64fc8c643ac0
Shaohan Yin <shaohan.yin@gmail.com>
2018-07-03 12:28:08 +0100

    [AZURE] Source upload to Azure File

--- azure_hpc_setup/azure_setup.py
+++ azure_hpc_setup/azure_setup.py
@@ -5,18 +5,22 @@ Script for Azure environment setup & task submission
 
 import configparser, os, time
 from azure.storage.blob import BlockBlobService, PublicAccess
+from azure.storage.file import FileService
 from azure.batch.batch_service_client import BatchServiceClient
 from azure.batch.batch_auth import SharedKeyCredentials
 from azure.batch.models import TaskAddParameter, MultiInstanceSettings, ResourceFile, UserIdentity, AutoUserScope, ElevationLevel, AutoUserSpecification
 
+# Configurations
 config_file = 'config.ini'
 config = configparser.ConfigParser()
 config.read(config_file)
 config_azure = config['AZURE']
 
+# Azure
 batch_credential = SharedKeyCredentials(config_azure['batch_account_name'], config_azure['batch_account_key'])
 batch_service = BatchServiceClient(batch_credential, config_azure['batch_account_url'])
 block_blob_service = BlockBlobService(account_name=config_azure['storage_account_name'], account_key=config_azure['storage_account_key'])
+file_service = FileService(account_name=config_azure['storage_account_name'], account_key=config_azure['storage_account_key'])
 
 def source_file_upload():
 	'''
@@ -25,8 +29,8 @@ def source_file_upload():
 	source_files = []
 	unique_files = []
 	for folder, _, files in os.walk('../'):
+		# Skip setup folder
 		if os.path.abspath(folder) == os.path.abspath('./'):
-			# Skip setup folder
 			continue
 
 		for file_name in files:
@@ -35,28 +39,34 @@ def source_file_upload():
 					source_files.append( (os.path.abspath(os.path.join(folder, file_name)), file_name) )
 					unique_files.append(file_name)
 
-	# Source File upload
-	block_blob_service.create_container(config_azure['input_container'], fail_on_exist=False, public_access=PublicAccess.Blob)
-	for path, blob_name in source_files:
-		print('Uploading file {} to container [{}]...'.format(blob_name, config_azure['input_container']))
-		block_blob_service.create_blob_from_path(config_azure['input_container'], blob_name, path)
+	# Source Upload
+	block_blob_service.create_container(config_azure['source_container'], fail_on_exist=False, public_access=PublicAccess.Blob)
+	for path, file_name in source_files:
+		print('Uploading file {} to container [{}]...'.format(file_name, config_azure['source_container']))
+		block_blob_service.create_blob_from_path(config_azure['source_container'], file_name, path)
+
+		if file_name == config_azure['source_file_name']:
+			print('Uploading file {} to file share [{}]...'.format(file_name, config_azure['source_share']))
+			file_service.create_file_from_path(config_azure['source_share'], None, file_name, path)
 
 def source_file_cleanup():
 	'''
 	Clean up source file from Azure
 	'''
-	print('Deleting container [{0}]...'.format(config_azure['input_container']))
-	block_blob_service.delete_container(config_azure['input_container'])
+	print('Deleting container [{0}]...'.format(config_azure['source_container']))
+	block_blob_service.delete_container(config_azure['source_container'])
+	print('Deleting file share [{0}]...'.format(config_azure['source_share']))
+	file_service.delete_share(config_azure['source_share'])
 	
 
-def task_submission():
+def task_submit():
 	'''
-	Automatic task submisson to Azure. Pool, VMs, Jobs should be created in advance.
+	Automatic task submission to Azure. Pool, VMs, Jobs should be created in advance.
 	'''
 	common_resource_files = [] 
 	for folder, _, files in os.walk('../'):
+		# Skip setup folder
 		if os.path.abspath(folder) == os.path.abspath('./'):
-			# Skip setup folder
 			continue
 
 		for file_name in files:
@@ -66,8 +76,10 @@ def task_submission():
 				print('Mapping {} to {}'.format(blob_url, file_path))
 				common_resource_files.append(ResourceFile(blob_url, file_path, file_mode='0775'))
 
+	command = '/usr/lib64/openmpi/bin/mpirun -mca btl_tcp_if_include eth0 -oversubscribe -n {0} -host $AZ_BATCH_HOST_LIST -wd $AZ_BATCH_TASK_SHARED_DIR python3 $AZ_BATCH_TASK_SHARED_DIR/bench.py'.format(config_azure['task_number_of_procs'])
+	coordination_command = 'echo'
 	multi_instance_settings = MultiInstanceSettings(
-		coordination_command_line='echo',
+		coordination_command_line=coordination_command,
 		number_of_instances=config_azure['task_number_of_instances'],
 		common_resource_files=common_resource_files
 	)
@@ -77,8 +89,8 @@ def task_submission():
 	))
 	task = TaskAddParameter(
 		id = 'benchtask',
-		command_line='/usr/lib64/openmpi/bin/mpirun -mca btl_tcp_if_include eth0 -oversubscribe -n {0} -host $AZ_BATCH_HOST_LIST -wd $AZ_BATCH_TASK_SHARED_DIR python3 $AZ_BATCH_TASK_SHARED_DIR/bench.py'.format(config_azure['task_number_of_procs']),
-		multi_instance_settings= multi_instance_settings,
+		command_line=command,
+		multi_instance_settings=multi_instance_settings,
 		user_identity=user
 	)
 
@@ -88,4 +100,4 @@ def task_submission():
 	batch_service.task.add(config_azure['job_id'], task)
 
 if __name__ == '__main__':
-	task_submission()
+	task_submit()
